3e2871a9d54da11daec2b99d6ed17b9ebdfe8456
Jinghua Li <jinhli@thoughtworks.com>
2023-05-24 11:13:50 +0800

    添加选择排序

--- src/main/java/algorithms/sort/Sort.java
+++ src/main/java/algorithms/sort/Sort.java
@@ -27,5 +27,23 @@ public final class Sort {
         return arr;
     }
 
+    public static int[] selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    minIndex = j;
+                    min = arr[j];
+                }
+            }
 
+            if (minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+
+        return arr;
+    }
 }
--- src/test/java/algorithms/sort/SortTest.java
+++ src/test/java/algorithms/sort/SortTest.java
@@ -7,10 +7,19 @@ import org.junit.jupiter.api.Test;
 class SortTest {
 
     @Test
-    void should_void_sort_by_bubble_sort() {
+    void should_void_sort_by_bubble() {
         int[] arr = {-1, -2, 3, 9, 8};
 
         int[] sortArr = Sort.bubbleSort(arr);
+        int[] except = {-2, -1, 3, 8, 9};
+        assertArrayEquals(except, sortArr);
+    }
+
+    @Test
+    void should_void_sort_by_select() {
+        int[] arr = {-1, -2, 3, 9, 8};
+
+        int[] sortArr = Sort.selectSort(arr);
         int[] except = {-2, -1, 3, 8, 9};
         assertArrayEquals(except, sortArr);
     }
