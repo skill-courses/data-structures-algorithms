5080deeded8d0333e239da5d74e64708b01dd5fd
Jinghua Li <jinhli@thoughtworks.com>
2023-06-05 18:25:32 +0800

    实现斐波那契查找算法

--- src/main/java/algorithms/search/Search.java
+++ src/main/java/algorithms/search/Search.java
@@ -47,4 +47,43 @@ public class Search {
 
         return -1;
     }
+
+    public int fibonacciSearch(int target) {
+        int arrSize = this.arr.length;
+        int fib2th = 0; // (m-2)'th Fibonacci number
+        int fib1th = 1; // (m-1)'th Fibonacci number
+        int fib = fib2th + fib1th; // m'th Fibonacci number
+
+        while (fib < arrSize) {
+            fib2th = fib1th;
+            fib1th = fib;
+            fib = fib2th + fib1th;
+        }
+
+        int offset = -1;
+
+        while (fib > 1) {
+            int index = Math.min(offset + fib2th, arrSize - 1);
+
+            if (arr[index] < target) {
+                fib = fib1th;
+                fib1th = fib2th;
+                fib2th = fib - fib1th;
+                offset = index;
+            } else if (arr[index] > target) {
+                fib = fib2th;
+                fib1th = fib1th - fib2th;
+                fib2th = fib - fib1th;
+            } else {
+                return index;
+            }
+        }
+
+        if (fib1th == 1 && arr[offset + 1] == target) {
+            return offset + 1;
+        }
+
+        return -1;
+    }
+
 }
--- src/test/java/algorithms/search/SearchTest.java
+++ src/test/java/algorithms/search/SearchTest.java
@@ -32,7 +32,29 @@ class SearchTest {
         assertEquals(-1, index2);
 
         final int index3 = search.insertSearch( 789);
+        assertEquals(-1, index3);
+    }
+
+    @Test
+    void should_can_search_by_fibonacci() {
+        int[] arr = new int[]{3, 5, 8, 10, 34, 567, 900, 1000};
+
+        final var search = new Search(arr);
+
+        final int index0 = search.fibonacciSearch(3);
+        assertEquals(0, index0);
+
+        final int index = search.fibonacciSearch(900);
+        assertEquals(6, index);
+
+        final int index2 = search.fibonacciSearch( 9000);
         assertEquals(-1, index2);
+
+        final int index3 = search.fibonacciSearch( 789);
+        assertEquals(-1, index3);
+
+        final int index4 = search.fibonacciSearch( 1000);
+        assertEquals(7, index4);
     }
 
 }
