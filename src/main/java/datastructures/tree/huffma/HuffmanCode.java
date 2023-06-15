e97fa6ebb5fc2e1bbfba23019aa92f5391662f8c
Jinghua Li <jinhli@thoughtworks.com>
2023-06-15 18:44:02 +0800

    完成赫夫曼解码

--- src/main/java/datastructures/tree/huffma/HuffmanCode.java
+++ src/main/java/datastructures/tree/huffma/HuffmanCode.java
@@ -1,12 +1,16 @@
 package datastructures.tree.huffma;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class HuffmanCode {
     private final Byte[] bytes;
     private final HuffmanTree tree;
+    private Map<Byte, String> codes;
 
     public HuffmanCode(String content) {
         this.bytes = IntStream.range(0, content.getBytes().length)
@@ -17,8 +21,17 @@ public class HuffmanCode {
     }
 
     public Byte[] zipToHuffmanCode() {
-        Map<Byte, String> codes = this.tree.buildHuffmaCodes();
+        this.codes = this.tree.buildHuffmaCodes();
         return zip(this.bytes, codes);
+    }
+
+    public String unZip(Byte[] huffmanBits) {
+        final var huffmanStr = this.parseToHuffmanStr(huffmanBits);
+        final var decode = this.decode(this.codes, huffmanStr);
+        return Arrays.stream(decode)
+                .filter(b -> b != null)
+                .map(b -> Character.toString((char) b.byteValue()))
+                .collect(Collectors.joining());
     }
 
     private Byte[] zip(Byte[] bytes, Map<Byte, String> codes) {
@@ -31,6 +44,48 @@ public class HuffmanCode {
                         Math.min((i + 1) * BYTE_LENGTH, huffmanStr.length())))
                 .map(sub -> (byte) Integer.parseInt(sub, TWO_RADIX))
                 .toArray(Byte[]::new);
+    }
+
+    private String byteToBitString(byte bit) {
+        return this.byteToBitString(bit, 8);
+    }
+
+    private String byteToBitString(byte bit, int length) {
+        int temp = bit;
+        temp |= (int)Math.pow(2, length);
+        final var str = Integer.toBinaryString(temp);
+        return str.substring(str.length() - length);
+    }
+
+    private String parseToHuffmanStr(Byte[] huffmanBytes) {
+        final var leftBytes = Arrays.copyOf(huffmanBytes, huffmanBytes.length - 1);
+        String huffmaStr =
+                Arrays.stream(leftBytes).map(this::byteToBitString).reduce("", (str1, str2) -> str1 + str2);
+        huffmaStr += byteToBitString(huffmanBytes[huffmanBytes.length - 1], 5);
+        return huffmaStr;
+    }
+
+    private Byte[] decode(Map<Byte, String> huffmaCodes, String huffmanStr) {
+        Map<String, Byte> swappedMap = huffmaCodes.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+
+        List<Byte> bytes = new ArrayList<>();
+        for (int i = 0; i < huffmanStr.length(); ) {
+            int count = 1;
+            boolean flag = true;
+            while (flag) {
+                String key = huffmanStr.substring(i, i + count);
+                if (!swappedMap.containsKey(key)) {
+                    count++;
+                } else {
+                    bytes.add(swappedMap.get(key));
+                    flag = false;
+                }
+            }
+            i += count;
+        }
+
+        return bytes.stream().toArray(Byte[]::new);
     }
 
 }
--- src/test/java/datastructures/tree/huffma/HuffmanCodeTest.java
+++ src/test/java/datastructures/tree/huffma/HuffmanCodeTest.java
@@ -8,12 +8,16 @@ class HuffmanCodeTest {
 
     @Test
     void should_zip_to_huffman_code() {
-        final var huffmanCode = new HuffmanCode("i like like like java do you like a java");
+        final var content = "i like like like java do you like a java";
+        final var huffmanCode = new HuffmanCode(content);
         final var bytes = huffmanCode.zipToHuffmanCode();
 
 //        Byte[] except = {-88, -65, -56, -65, -56, -65, -55, 77, -57, 6, -24, -14, -117, -4, -60, -90, 28};
         Byte[] except = {-87, -67, -23, -67, -23, -67, -24, -24, -52, -127, -104, 39, 77, -17, 114, 58, 6};
         assertArrayEquals(except, bytes);
+
+        final var exceptStr = huffmanCode.unZip(except);
+        assertEquals(content, exceptStr);
     }
 
 }
